3d7f44c013ea178cba0378962a1c7b74860dc3d1
Samantha Crespo <samkcrespo@gmail.com>
2022-06-07 20:48:17 -0700

    commit tree c890f4ca20b228d30971be8d2c7bdd5677c0b718

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,9 +6,18 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  # def create
+  #   movie = Movie.create(movie_params)
+  #   render json: movie, status: :created
+  # rescue ActiveRecord::RecordInvalid => e
+  #   render json: {errors: e.record.errors.full_messages }, status: :unprocessable_entity
+  # end
+
   def create
-    movie = Movie.create(movie_params)
+    movie = Movie.create!(movie_params)
     render json: movie, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
   end
 
   private
